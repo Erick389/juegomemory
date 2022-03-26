08720004a0696bd2c31012c59b67b72626382304
Erick389 <102175247+Erick389@users.noreply.github.com>
2022-03-25 18:13:30 -0600

    complete

--- memory.py
+++ memory.py
@@ -1,14 +1,29 @@
+"""Memory, puzzle game of number pairs.
+
+Exercises:
+
+1. Count and print how many taps occur.
+2. Decrease the number of tiles to a 4x4 grid.
+3. Detect when all tiles are revealed.
+4. Center single-digit tile.
+5. Use letters instead of tiles.
+"""
+
+from email.errors import NoBoundaryInMultipartDefect
 from random import *
 from turtle import *
+
 from freegames import path
 
 car = path('car.gif')
 tiles = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P"]*4
 state = {'mark': None}
 hide = [True] * 64
+contaps=0
+numaciertos=0
 
 def square(x, y):
-    "Draw white square with black outline at (x, y)."
+    """Draw white square with black outline at (x, y)."""
     up()
     goto(x, y)
     down()
@@ -19,28 +34,46 @@ def square(x, y):
         left(90)
     end_fill()
 
+
 def index(x, y):
-    "Convert (x, y) coordinates to tiles index."
+    """Convert (x, y) coordinates to tiles index."""
     return int((x + 200) // 50 + ((y + 200) // 50) * 8)
 
+
 def xy(count):
-    "Convert tiles count to (x, y) coordinates."
+    """Convert tiles count to (x, y) coordinates."""
     return (count % 8) * 50 - 200, (count // 8) * 50 - 200
 
+
 def tap(x, y):
-    "Update mark and hidden tiles based on tap."
+    """Update mark and hidden tiles based on tap."""
     spot = index(x, y)
     mark = state['mark']
 
     if mark is None or mark == spot or tiles[mark] != tiles[spot]:
         state['mark'] = spot
+        global contaps
+        contaps+=1
+        print(contaps)
     else:
         hide[spot] = False
         hide[mark] = False
         state['mark'] = None
+        global numaciertos
+        numaciertos+=1
+        print(contaps)
+        if numaciertos == 32:
+            print("ganaste")
+        up()
+        goto(0, 600)
+        color('black')
+        write(numaciertos, font=('Arial', 30, 'normal'))
+        
+        
+        
 
 def draw():
-    "Draw image and tiles."
+    """Draw image and tiles."""
     clear()
     goto(0, 0)
     shape(car)
@@ -56,15 +89,19 @@ def draw():
     if mark is not None and hide[mark]:
         x, y = xy(mark)
         up()
-        goto(x + 8, y+6)#centrar numeros 
-        color('blue')
+        goto(x + 10, y+6)
+        color('black')
         write(tiles[mark], font=('Arial', 30, 'normal'))
-
+    up()
+    goto(0, 300)
+    color('black')
+    write(contaps, font=('Arial', 30, 'normal'))
     update()
     ontimer(draw, 100)
 
+
 shuffle(tiles)
-setup(420, 420, 370, 0)
+setup(700, 700, 370, 0)
 addshape(car)
 hideturtle()
 tracer(False)
